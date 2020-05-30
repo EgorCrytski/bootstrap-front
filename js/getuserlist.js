64a8f6cd0fd4bcb8ef3420dec619a1b7edc9d207
Egor Crytski <egorcrytski@gmail.com>
2020-05-30 16:50:59 +0300

    create table from json data (does not work)

--- js/getuserlist.js
+++ js/getuserlist.js
@@ -4,13 +4,18 @@ var url  = "http://localhost:8000/api/v1/library/user/all/";
 var xhr  = new XMLHttpRequest();
 xhr.open('Get', url, false);
 xhr.setRequestHeader("Authorization", "JWT "+JSON.parse(sessionStorage.getItem("access")));
+jsondata = null;
 xhr.onload = function () {
     var users = JSON.parse(xhr.responseText);
     if (xhr.readyState == 4 && xhr.status == "200") {
         console.table(users);
+        alert(xhr.responseText);
+        jsondata = users;
     } else {
         console.error(users);
+        jsondata = users;
     }
 }
 xhr.send(null);
+return jsondata;
 }
