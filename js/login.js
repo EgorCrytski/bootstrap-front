abec52d497c09681573575f0ca3f5a50d571730f
Egor Crytski <egorcrytski@gmail.com>
2020-06-10 10:18:16 +0300

    logout

--- js/login.js
+++ js/login.js
@@ -19,3 +19,36 @@ xhr.onreadystatechange = function() {
 
 xhr.send(JSON.stringify({username: log, password: pwd}));
 }
+
+function currentuser()
+{
+var url  = "http://localhost:8000/api/v1/library/user/me/";
+var xhr  = new XMLHttpRequest();
+xhr.open('Get', url, false);
+xhr.setRequestHeader("Authorization", "JWT "+JSON.parse(sessionStorage.getItem("access")));
+
+xhr.onload = function () {
+    var user = JSON.parse(xhr.responseText);
+    var txt ='';
+    if (xhr.readyState == 4 && xhr.status == "200") {
+        txt +="<h4>Logged as: "+user.username+"</h4>";
+        txt +="<button class='btn btn-outline-success my-2 my-sm-0' onclick='logout()'>Logout</button>";
+        document.getElementById('loginform').innerHTML = txt;
+        console.table(user);
+        jsondata = user;
+    } else {
+        txt +="<input class='form-control mr-sm-2' type='login' name='log' placeholder='Login' aria-label='Login'><input class='form-control mr-sm-2' type='password' name='pwd' placeholder='Password' aria-label='Password'><button class='btn btn-outline-success my-2 my-sm-0' type='submit' onclick='login(this.form.log.value, this.form.pwd.value)'>Login</button>";
+                document.getElementById('loginform').innerHTML = txt;
+
+        console.error(user);
+
+    }
+}
+xhr.send(null);
+}
+
+function logout()
+{
+    sessionStorage.setItem('access', null);
+    sessionStorage.setItem('refresh', null);
+}
